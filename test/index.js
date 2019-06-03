23c152831f762fc04cc049ee09b25fc38ab7ab00
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-03 14:57:06 -0700

    added firebase to test/index.js

--- test/index.js
+++ test/index.js
@@ -12,6 +12,7 @@ global.FormData = window.FormData;
 global.localStorage = window.localStorage;
 global.sessionStorage = window.sessionStorage;
 global.URLSearchParams = window.URLSearchParams;
+global.firebase = require('firebase');
 
 require = require('esm')(module);
 module.exports = require('./tests.js');
