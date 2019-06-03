7f2bd6b27f8639f248f48079cda7770132c47255
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-03 13:56:00 -0700

    firebase authorization

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,15 @@
 import '../utils/check-auth.js';
 import App from './App.js';
+import { auth } from '../services/firebase.js';
+
+auth.onAuthStateChanged(user => {
+    if(user) {
+        console.log('we have a user');
+    }
+    else {
+        console.log('no user');
+    }
+});
 
 const root = document.getElementById('app');
 const app = new App();
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,13 @@
-onst config = {
-    apiKey: "AIzaSyBFpUAntyJ_J7g3mCpYEiO2N4AbLRwrBlw",
-   authDomain: "movie-list-ebd83.firebaseapp.com",
-   databaseURL: "https://movie-list-ebd83.firebaseio.com",
-   projectId: "movie-list-ebd83",
-   storageBucket: "movie-list-ebd83.appspot.com",
-   messagingSenderId: "490440378511",
-   appId: "1:490440378511:web:f8f54d5acce5e35f"
+const config = {
+    apiKey: 'AIzaSyBFpUAntyJ_J7g3mCpYEiO2N4AbLRwrBlw',
+    authDomain: 'movie-list-ebd83.firebaseapp.com',
+    databaseURL: 'https://movie-list-ebd83.firebaseio.com',
+    projectId: 'movie-list-ebd83',
+    storageBucket: 'movie-list-ebd83.appspot.com',
+    messagingSenderId: '490440378511',
+    appId: '1:490440378511:web:f8f54d5acce5e35f'
 };
 
 firebase.initializeApp(config);
-
+// export const db = firebase.database();
 export const auth = firebase.auth();
