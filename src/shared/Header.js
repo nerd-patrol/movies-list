e9516ea75500b1f7bc60ccd87a2c53fe40fd0a7b
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-03 14:53:47 -0700

    render profile via header

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,7 +1,20 @@
 import Component from '../Component.js';
+import Profile from './Profile.js';
+import { auth } from '../services/firebase.js';
 
 class Header extends Component {
 
+    render() {
+        const dom = this.renderDOM();
+        const profile = new Profile();
+        dom.appendChild(profile.render());
+
+        auth.onAuthStateChanged(user => {
+            profile.update({ user });
+        });
+
+        return dom;
+    }
 
     renderTemplate() {
         return /*html*/ `
