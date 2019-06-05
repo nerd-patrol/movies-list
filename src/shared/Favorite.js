c83b8805070f938b87f70855f75d63b4ee8ef7ba
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-06-04 17:38:17 -0700

    rendered favorite star button to MovieItem

---
+++ src/shared/Favorite.js
@@ -0,0 +1,24 @@
+import Component from '../Component.js';
+
+class Favorite extends Component {
+
+    render() {
+        const button = this.renderDOM();
+        button.addEventListener('click', () => {
+            this.props.onFavorite(!this.props.isFavorite);
+        });
+        
+        return button;
+    }
+
+    renderTemplate() {
+        const isFavorite = this.props.isFavorite;
+        const star = isFavorite ? '★' : '☆';
+
+        return /*html*/ `
+            <button class="favorite-star ${isFavorite ? 'favorite' : ''}">${star}</button> 
+        `;
+    }
+}
+
+export default Favorite;
--- src/shared/MovieItem.js
+++ src/shared/MovieItem.js
@@ -1,12 +1,27 @@
 import Component from '../Component.js';
+import Favorite from '../shared/Favorite.js';
 
 class MovieItem extends Component {
 
+    render() {
+        const dom = this.renderDOM();
+        const movie = this.props.movie;
+
+        const container = dom.querySelector('.favorite-container');
+        const favorite = new Favorite();
+
+        // })
+
+        container.appendChild(favorite.render());
+
+        return dom;
+    }
 
     renderTemplate() {
         const movie = this.props.movie;
         return /*html*/ `
             <li>
+                <div class="favorite-container"></div>
                 <p class="movie-title">${movie.title}</p>
                 <img src="http://image.tmdb.org/t/p/w200${movie.poster_path}" alt="${movie.title} movie poster">
             </li>
