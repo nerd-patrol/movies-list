d34a7f50558b487243320302525e32bbf51b7fc3
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-06 14:43:45 -0700

    render userlist and user item

---
+++ src/users/UserList.js
@@ -0,0 +1,26 @@
+import Component from '../Component.js';
+import UserItem from './UserItem.js';
+
+class UserList extends Component {
+
+    render() {
+        const list = this.renderDOM();
+        const users = this.props.users;
+
+        users.forEach(user => {
+            const userItem = new UserItem({ user });
+            list.appendChild(userItem.render());
+        });
+
+        return list;
+    }
+
+    renderTemplate() {
+        return /*html*/ `
+            <ul class="user-list">
+            </ul>
+        `;
+    }
+}
+
+export default UserList;
