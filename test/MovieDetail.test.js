6222cf29ac7e03abfdda974603e70a674c549fc5
Noah Puckett <noah.ingram.puckett@gmail.com>
2019-06-06 14:43:21 -0700

    wokring tests

--- test/MovieDetail.test.js
+++ test/MovieDetail.test.js
@@ -7,7 +7,8 @@ test('renders. at all.', assert => {
     // arrange
     const movie = {
         title: 'Aladdin',
-        poster_path: '3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg'
+        poster_path: '3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg',
+        overview: '"A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true."'
     };
 
     const movieDetail = new MovieDetail({ movie });
@@ -16,6 +17,8 @@ test('renders. at all.', assert => {
         <section>
             <h2>Aladdin</h2>
             <img src="http://image.tmdb.org/t/p/w200/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg">
+            <p>"A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true."</p>
+            <div class="favorite-container"></div>
         </section>
     `;
 
--- test/tests.js
+++ test/tests.js
@@ -2,6 +2,7 @@ import { app } from '../src/services/firebase.js';
 import './html-equal.js';
 import './Profile.test.js';
 import './MovieDetail.test.js';
+import './useritem.test.js';
 
 QUnit.done(() => {
     app.delete();
